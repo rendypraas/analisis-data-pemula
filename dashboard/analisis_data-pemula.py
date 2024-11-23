4df93578bd771f803d6d33cfc96e1830333a6488
Rendy Prasetya <110278525+rendypraas@users.noreply.github.com>
2024-11-23 14:33:29 +0700

    Update analisis_data-pemula.py

--- dashboard/analisis_data-pemula.py
+++ dashboard/analisis_data-pemula.py
@@ -32,9 +32,6 @@ import matplotlib.image as mpimg
 import seaborn as sns
 import urllib.request
 import streamlit as st
-import geopandas as gpd
-from geodatasets import get_path
-import time
 
 """## Data Wrangling
 
